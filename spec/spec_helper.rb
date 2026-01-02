c9639e8e3e1dcebdaed2acf69f63bb72d433d851
Miles Georgi <azimux@gmail.com>
2026-01-02 11:21:54 -0800

    Update vcr config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,10 +9,20 @@ require "simplecov"
 require "vcr"
 
 VCR.configure do |config|
+  config.filter_sensitive_data("<OPEN_AI_KEY>") { ENV["OPENAI_API_KEY"] }
+
+  config.before_record do |interaction|
+    if interaction.request.headers["Cookie"]
+      interaction.request.headers["Cookie"] = ["<SCRUBBED>"]
+    end
+    if interaction.response.headers["Set-Cookie"]
+      interaction.response.headers["Set-Cookie"] = ["<SCRUBBED>"]
+    end
+  end
+
   config.cassette_library_dir = "spec/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.filter_sensitive_data("<OPEN_AI_KEY>") { ENV.fetch("OPENAI_API_KEY", nil) }
 end
 
 SimpleCov.start do
